eeb8cbf2aa83fe75e48670333d83676096f93d2c
Damian <robocikd@wp.pl>
2023-07-28 14:42:26 +0200

    Replace mock with ExtendsWith, Mock, InjectMoc annotations

--- src/test/java/pl/robocikd/testud/service/EmployeeServiceImplTest.java
+++ src/test/java/pl/robocikd/testud/service/EmployeeServiceImplTest.java
@@ -2,39 +2,44 @@ package pl.robocikd.testud.service;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.mockito.BDDMockito;
-import org.mockito.Mockito;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 import pl.robocikd.testud.model.Employee;
 import pl.robocikd.testud.repository.EmployeeRepository;
-import pl.robocikd.testud.service.impl.EmployeeService;
 
 import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.BDDMockito.given;
 
+@ExtendWith(MockitoExtension.class)
 class EmployeeServiceImplTest {
 
+    @Mock
     private EmployeeRepository employeeRepository;
 
-    private EmployeeService employeeService;
+    @InjectMocks
+    private EmployeeServiceImpl employeeService;
+
+    private Employee employee;
 
     @BeforeEach
     void setup() {
-        employeeRepository = Mockito.mock(EmployeeRepository.class);
-        employeeService = new EmployeeServiceImpl(employeeRepository);
-    }
-
-    @Test
-    void givenEmployee_whenSaveEmployee_thenReturnEmployee() {
-        // given
-        Employee employee = Employee.builder()
+        employee = Employee.builder()
                 .id(1L)
                 .firstName("Damian")
                 .lastName("Robot")
                 .email("daro@gmail.com")
                 .build();
-        BDDMockito.given(employeeRepository.findByEmail(employee.getEmail())).willReturn(Optional.empty());
-        BDDMockito.given(employeeRepository.save(employee)).willReturn(employee);
+    }
+
+    @Test
+    void givenEmployee_whenSaveEmployee_thenReturnEmployee() {
+        // given
+        given(employeeRepository.findByEmail(employee.getEmail())).willReturn(Optional.empty());
+        given(employeeRepository.save(employee)).willReturn(employee);
         // when
         Employee savedEmployee = employeeService.saveEmployee(employee);
         // then
